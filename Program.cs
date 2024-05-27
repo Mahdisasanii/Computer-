107cc7a612b59b3c9991e1202526b6292003bd2e
Mahdisasanii <erfanssnii@gmail.com>
2024-05-27 20:20:59 +0330

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp37
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter num1:");
+            double num1 = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Enter num2:");
+            double num2 = Convert.ToDouble(Console.ReadLine());
+            double avg = (num1 + num2) / 2;
+            if (avg > num1)
+            {
+                Console.WriteLine("yes");
+            }
+            else
+            {
+                Console.WriteLine("no");
+            }
+            Console.ReadKey();
+
+        }
+    }
+}
